270fa95f8aa157f87d23ea549f4687b00daf3dda
ch sarfraj <sarfrajch370@live.com>
2019-10-24 17:05:41 +0530

    Add AccomodationPackage edit delete functionality

--- HotelManagementSystem/Areas/Deshboard/ViewModels/AccomodationTypesListingModel.cs
+++ HotelManagementSystem/Areas/Deshboard/ViewModels/AccomodationTypesListingModel.cs
@@ -7,26 +7,15 @@ using System.Web;
 namespace HotelManagementSystem.Areas.Deshboard.ViewModels
 {
     public class AccomodationTypesListingModel
-
     {
-
-        public IEnumerable<AccomodationType> AccomodationTypes { get; set; }
-
+       public IEnumerable<AccomodationType> AccomodationTypes { get; set; }
         public string SearchTerm { get; set; }
-
     }
 
-
-
     public class AccomodationTypeActionModel
-
     {
-
         public int ID { get; set; }
-
         public string Name { get; set; }
-
         public string Description { get; set; }
-
     }
 }
